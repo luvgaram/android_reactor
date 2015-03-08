b203b97a96c7fc176417125ba1c6a7afe3b45496
luvgaram <garam@nhnnext.org>
2015-03-08 23:03:37 +0900

    reactor 구현

---
+++ src/ServerListData.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+import org.simpleframework.xml.ElementList;
+
+
+public class ServerListData {
+
+	// ElementList로 server를 지정
+	@ElementList(entry="server", inline=true)
+	private List<HandlerListData> server;
+	
+	public List<HandlerListData> getServer() {
+		return server;
+	}
+}
