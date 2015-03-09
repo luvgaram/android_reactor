4286437d38a3e8bf5ee3bf438a671db8ea1a1631
luvgaram <garam@nhnnext.org>
2015-03-09 10:24:36 +0900

    thread 구현

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -10,27 +10,32 @@ public class Dispatcher {
 	public void dispatch(ServerSocket serverSocket, HandleMap handleMap) {
 		try {
 			Socket socket = serverSocket.accept();
-			demultiplex(socket, handleMap);
+			
+			Runnable demultiplexer = new Demultiplexer(socket, handleMap);
+			Thread thread = new Thread(demultiplexer);
+			thread.start();
+			
+//			demultiplex(socket, handleMap);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}	
 	}
 	
-	public void demultiplex(Socket socket, HandleMap handleMap) {
-		try {
-			InputStream inputStream = socket.getInputStream();
-			
-			byte[] buffer = new byte[HEADER_SIZE];
-			inputStream.read(buffer);
-			String header = new String(buffer);
-			
-			// HandleMap을 받아서 헤더값으로 get한 EventHandler에게 이벤트 발생
-			handleMap.get(header).handleEvent(inputStream);
-			
-			// 데이터 처리 후 접속 종료
-			socket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+//	public void demultiplex(Socket socket, HandleMap handleMap) {
+//		try {
+//			InputStream inputStream = socket.getInputStream();
+//			
+//			byte[] buffer = new byte[HEADER_SIZE];
+//			inputStream.read(buffer);
+//			String header = new String(buffer);
+//			
+//			// HandleMap을 받아서 헤더값으로 get한 EventHandler에게 이벤트 발생
+//			handleMap.get(header).handleEvent(inputStream);
+//			
+//			// 데이터 처리 후 접속 종료
+//			socket.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
